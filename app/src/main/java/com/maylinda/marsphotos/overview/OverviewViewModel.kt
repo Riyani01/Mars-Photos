d677e5f66a0d69e029e794f0dab628672ae3f40d
MayLinda06 <edihermawan1223@gmail.com>
2022-11-10 11:37:08 +0700

    awal

---
+++ app/src/main/java/com/maylinda/marsphotos/overview/OverviewViewModel.kt
@@ -0,0 +1,54 @@
+package com.maylinda.marsphotos.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maylinda.marsphotos.network.MarsApi
+import com.maylinda.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
+
+//class overviewViewModel->mengakses api service hanya dijalankan pertama kali
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<MarsApiStatus>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<MarsApiStatus> = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
+    // with new values
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
+    /**
+     * Call getMarsPhotos() on init so we can display status immediately.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    //try ->mengaktifkan retrofit yang menghasilkan  list
+
+    private fun getMarsPhotos() {
+
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
+    }
+}
